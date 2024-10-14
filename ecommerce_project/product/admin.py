7553edd96ecb75b6342fe6d8859347ac0a79f133
ron199807 <ronaldmweema@gmail.com>
2024-10-14 18:49:12 +0200

    validated the rating from 1 to 5

--- ecommerce_project/product/admin.py
+++ ecommerce_project/product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User 
+from .models import CustomUser, Category, Product, Order, Wishlist, ProductImage, Review
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(CustomUser)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Wishlist)
+admin.site.register(ProductImage)
+admin.site.register(Review)
+
--- ecommerce_project/product/serializers.py
+++ ecommerce_project/product/serializers.py
@@ -37,6 +37,11 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'product', 'user', 'rating', 'comment', 'created_at']
         read_only_fields = ['id', 'created_at']
 
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5")
+        return value
+
 # product images serializer
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
